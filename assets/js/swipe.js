b2f2d9ec3a6e1ce66c0c8cbd4004f5e2d1ddf6b7
nhavantuonglai <info@nhavantuonglai.com>
2023-12-16 11:01:56 +0700

    5423

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -19,34 +19,34 @@
 const d = document;
 d.addEventListener("DOMContentLoaded", function(event) {
 
-    if (d.querySelector('.headroom')) {
-        var headroom = new Headroom(document.querySelector("#navbar-main"), {
-            offset: 0,
-            tolerance: {
-                up: 0,
-                down: 0
-            },
-        });
-        headroom.init();
-    }
+	if (d.querySelector('.headroom')) {
+		var headroom = new Headroom(document.querySelector("#navbar-main"), {
+			offset: 0,
+			tolerance: {
+				up: 0,
+				down: 0
+			},
+		});
+		headroom.init();
+	}
 
-    // Tooltips
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+	// Tooltips
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl)
+	})
 
-    // Popovers
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-    })
+	// Popovers
+	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+	return new bootstrap.Popover(popoverTriggerEl)
+	})
 
-    var scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 500,
-        speedAsDuration: true
-    });
+	var scroll = new SmoothScroll('a[href*="#"]', {
+		speed: 500,
+		speedAsDuration: true
+	});
 
-    d.querySelector('.current-year').textContent = new Date().getFullYear();
+	d.querySelector('.current-year').textContent = new Date().getFullYear();
 
 });
